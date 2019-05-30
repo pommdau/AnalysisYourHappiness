9eea95c43e945d184a2640849e95a8c1fd76dc22
hikeuchi <hikeuchi@morisawa.co.jp>
2019-05-30 16:23:48 +0900

    UIの調整

--- AnalysisYourHappiness/AnalysisYourHappiness/HappinessItem.swift
+++ AnalysisYourHappiness/AnalysisYourHappiness/HappinessItem.swift
@@ -14,11 +14,11 @@ class HappinessItem: NSObject, Codable {
     var rating = 3.0
     var time   = 3.0  // 幸せ持続時間（hour）
     var price  = 1000 // かかる費用(yen)
-    var costPerformance: Double {   // コスパ = レーティンratingSlider.valueグ * 持続時間 / 費用
+    var costPerformance: Double {   // コストパフォーマンス = レーティング * 持続時間 / 費用
         get {
             let fixedRating = rating * 1000     // レートの補正
             if price == 0 {
-                return fixedRating * time / 1.0     // 0円で割ると不定になるので0円の場合は1円とする
+                return fixedRating * time / 1.0     // 0円で割ると不定になるので1円とする
             }
             return fixedRating * time / Double(price)
         }
